0762ac549fe22eebdbc9b932df8d2a51669ddd19
Unknown <talles_of_destiny@yahoo.com.sg>
2019-02-24 19:26:43 +0900

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick(string btn_name) {
+        Debug.Log("test click");
+        if (btn_name == "START") {
+            SceneManager.LoadScene("TestNextScene", LoadSceneMode.Single);
+        }
+    }
+}
