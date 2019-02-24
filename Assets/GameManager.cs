964a2b60fed35f4cd74b24b61219e56078038dc3
Unknown <talles_of_destiny@yahoo.com.sg>
2019-02-25 00:46:38 +0900

    Added text score

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int score;
+    private Text txt;
+
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;   
+        score = 0; 
+        txt = GameObject.Find("Text (1)").GetComponent<Text>();
     }
 
     public void updateScore(){
         score++;
+        txt.text = score.ToString();
         Debug.Log("Score is " + score);
     }
 }
